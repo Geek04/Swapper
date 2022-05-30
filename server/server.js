05a8a9729c61afcbaaa3ade7f358850b1af77f50
Haswell <selimkayumov@gmail.com>
2022-05-30 13:54:08 +0200

    Fixed bug with unsuccesful about page refreshes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const connectHistory = require('connect-history-api-fallback');
+
 const path = require("path");
-const token_update = require("./token_update")
+
+const token_update = require("./token_update");
 
 const tokenUpdateInterval = 60000;
 
@@ -17,6 +20,8 @@ mongoose.connect("mongodb://localhost:27017/swapper-main", { useNewUrlParser: tr
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(morgan("dev"));
+
+app.use(connectHistory());
 
 app.use("/api/tokens", require("./routes/tokens"));
 app.use("/", express.static(path.join(__dirname, "../dist")));
